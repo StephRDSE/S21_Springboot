7b53b98ce374e13f0bc747697293eaeefc1a618e
Utilisateur <stephane.rodolausse.pf1@gmail.com>
2018-05-22 11:09:34 +0200

    Commit de départ avec tous les fichiers

---
+++ src/main/java/co/simplon/crud/DemoApplication.java
@@ -0,0 +1,12 @@
+package co.simplon.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages= "co.simplon.crud")
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
